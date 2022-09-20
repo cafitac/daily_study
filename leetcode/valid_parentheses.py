bb04ad295422bf450d57cec3ac25208f34324b6c
cafitac <cafitac99@gmail.com>
2022-09-20 20:14:24 +0900

    괄호 검증 더 좋은 풀이

--- leetcode/valid_parentheses.py
+++ leetcode/valid_parentheses.py
@@ -26,6 +26,22 @@ class Solution:
                     return False
         return len(st) == 0
 
+    def isValid2(self, s: str) -> bool:
+        st = list()
+
+        open_list = ["(", "[", "{"]
+        close_list = [")", "]", "}"]
+
+        for c in s:
+            if c in open_list:
+                st.append(c)
+                continue
+
+            if c in close_list:
+                if st == [] or close_list.index(c) != open_list.index(st.pop()):
+                    return False
+        return st == []
+
 
 if __name__ == '__main__':
     solution = Solution()
@@ -34,3 +50,9 @@ if __name__ == '__main__':
     assert solution.isValid("(]") is False
     assert solution.isValid("[") is False
     assert solution.isValid("]") is False
+
+    assert solution.isValid2("()") is True
+    assert solution.isValid2("()[]{}") is True
+    assert solution.isValid2("(]") is False
+    assert solution.isValid2("[") is False
+    assert solution.isValid2("]") is False

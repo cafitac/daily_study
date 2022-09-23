44dcad39f4924c6e575337089325ca9f752410f7
cafitac <cafitac99@gmail.com>
2022-09-23 11:55:31 +0900

    특정 숫자 제거하기 다른 해설 추가

--- leetcode/remove_element.py
+++ leetcode/remove_element.py
@@ -8,8 +8,17 @@ class Solution:
                 nums.pop(idx)
         return len(nums)
 
+    def removeElement2(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            print(nums)
+            nums.remove(val)
+        return len(nums)
+
 
 if __name__ == '__main__':
     solution = Solution()
     assert solution.removeElement([3, 2, 2, 3], 3) == 2
     assert solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2) == 5
+
+    assert solution.removeElement2([3, 2, 2, 3], 3) == 2
+    assert solution.removeElement2([0, 1, 2, 2, 3, 0, 4, 2], 2) == 5

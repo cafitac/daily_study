43128d8f2a90e610424a7f402e93dd1aea71b842
cafitac <cafitac99@gmail.com>
2023-10-04 17:26:07 +0900

    Threadpool 적용

--- fastcampus/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
+++ fastcampus/was-practice/src/main/java/org/example/CustomWebApplicationServer.java
@@ -3,12 +3,16 @@ package org.example;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class CustomWebApplicationServer {
 
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -29,7 +33,7 @@ public class CustomWebApplicationServer {
                 /**
                  * Step2 - 사용자 요청이 들어올 때마다 Thread 를 새로 생성해서 사용자 요청을 처리하도록 한다.
                  */
-                new Thread(new ClientRequestHandler(clientSocket)).start();
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }

65647693ca244d140d40d30b2677e6c4e17661eb
cafitac <cafitac99@gmail.com>
2023-10-06 15:00:36 +0900

    HttpServlet 으로 변경

--- fastcampus/servlet/src/main/java/org/example/CalculatorServlet.java
+++ fastcampus/servlet/src/main/java/org/example/CalculatorServlet.java
@@ -2,25 +2,23 @@ package org.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.GenericServlet;
-import javax.servlet.Servlet;
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.example.calculator.calculate.domain.Calculator;
 import org.example.calculator.calculate.domain.PositiveNumber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @WebServlet(urlPatterns = "/calculate")
-public class CalculatorServlet extends GenericServlet {
+public class CalculatorServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(CalculatorServlet.class);
 
     @Override
-    public void service(final ServletRequest request, final ServletResponse response)
+    protected void doGet(final HttpServletRequest request, final HttpServletResponse response)
         throws ServletException, IOException {
         log.info("service");
         final int operand1 = Integer.parseInt(request.getParameter("operand1"));

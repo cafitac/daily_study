b13f2115c3a8550388e205975cdd0a5cd54afc05
cafitac <cafitac99@gmail.com>
2023-11-09 13:58:48 +0900

    어드민 프로젝트 생성

---
+++ fastcampus/e-commerce/hello-ecommer-admin/src/main/java/com/fastcampus/helloecommeradmin/HelloEcommerAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.helloecommeradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEcommerAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEcommerAdminApplication.class, args);
+	}
+
+}
---
+++ fastcampus/e-commerce/hello-ecommer-admin/src/test/java/com/fastcampus/helloecommeradmin/HelloEcommerAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.helloecommeradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloEcommerAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

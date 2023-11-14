1b32b703f2a0777df46511c68daa467aee36d193
cafitac <cafitac99@gmail.com>
2023-11-14 16:59:00 +0900

    PostRepository 테스트 케이스 등록

---
+++ fastcampus/high-volume-data&traffic/fastcampus-mysql-init-project/src/test/java/com/example/fastcampusmysql/domain/post/PostRepositoryTest.java
@@ -0,0 +1,100 @@
+package com.example.fastcampusmysql.domain.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class PostRepositoryTest {
+
+    @Test
+    void groupByCreatedDate() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findAllByInId() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findAllByMemberId() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findById() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findAllByMemberIdOrderByIdDesc() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findAllByInMemberIdOrderByIdDesc() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findAllByLessThenIdAndMemberIdOrderByIdDesc() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void findAllByLessThenIdAndInMemberIdOrderByIdDesc() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void saveInsert() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void saveUpdate() {
+        // given
+
+        // when
+
+        // then
+    }
+}

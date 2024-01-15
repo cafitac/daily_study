5063041c95aecbf4af0a03407d6ecfdb96f91a19
cafitac <cafitac99@gmail.com>
2024-01-15 20:23:16 +0900

    test: 테스트 코드 작성

--- fastcampus/high-volume-data&traffic/fastcampus-mysql-init-project/src/test/java/com/example/fastcampusmysql/domain/post/PostRepositoryTest.java
+++ fastcampus/high-volume-data&traffic/fastcampus-mysql-init-project/src/test/java/com/example/fastcampusmysql/domain/post/PostRepositoryTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -59,38 +60,61 @@ public class PostRepositoryTest {
 
     @Test
     void findAllByMemberId() {
-        // given
+        var post1 = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        var post2 = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        var post3 = PostFixtureFactory.getByCreatedDate(2L, LocalDate.of(1999, 12, 31)).nextObject(Post.class);
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
 
-        // when
+        var findPosts = postRepository.findAllByMemberId(1L, PageRequest.of(0, 10));
 
-        // then
+        assertThat(findPosts).hasSize(2);
     }
 
     @Test
     void findById() {
-        // given
+        var post = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        postRepository.save(post);
 
-        // when
+        final Post savedPost = postRepository.findAllByMemberIdOrderByIdDesc(1L, 1)
+            .stream()
+            .findFirst()
+            .get();
 
-        // then
+        final Post findPost = postRepository.findById(savedPost.getId(), false).get();
+
+        assertThat(findPost.getContents()).isEqualTo(savedPost.getContents());
     }
 
     @Test
     void findAllByMemberIdOrderByIdDesc() {
-        // given
+        var post1 = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        var post2 = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        var post3 = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 31)).nextObject(Post.class);
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
 
-        // when
+        final List<Post> findPosts = postRepository.findAllByMemberIdOrderByIdDesc(
+            1L, 3);
 
-        // then
+        assertThat(findPosts).hasSize(3);
     }
 
     @Test
     void findAllByInMemberIdOrderByIdDesc() {
-        // given
+        var post1 = PostFixtureFactory.getByCreatedDate(1L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        var post2 = PostFixtureFactory.getByCreatedDate(2L, LocalDate.of(1999, 12, 30)).nextObject(Post.class);
+        var post3 = PostFixtureFactory.getByCreatedDate(3L, LocalDate.of(1999, 12, 31)).nextObject(Post.class);
+        postRepository.save(post1);
+        postRepository.save(post2);
+        postRepository.save(post3);
 
-        // when
+        final List<Post> findPosts = postRepository.findAllByInMemberIdOrderByIdDesc(
+            List.of(1L, 2L, 3L), 3);
 
-        // then
+        assertThat(findPosts).hasSize(3);
     }
 
     @Test

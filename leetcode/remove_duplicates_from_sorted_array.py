0c86a54f1edbad27f09d50f24897a513ce130509
cafitac <cafitac99@gmail.com>
2024-01-23 20:15:51 +0900

    26. Remove Duplicates from Sorted Array

--- leetcode/remove_duplicates_from_sorted_array.py
+++ leetcode/remove_duplicates_from_sorted_array.py
@@ -2,14 +2,27 @@ from typing import List
 
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> List:
-        for idx in range(len(nums)-1, 0, -1):
-            if nums[idx] == nums[idx-1]:
-                nums.pop(idx-1)
-        return nums
+    def removeDuplicates(self, nums: List[int]) -> int:
+        nums[:] = sorted(set(nums))
+        return len(nums)
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+
+        while len(nums) > i:
+            if nums[i] == nums[i-1]:
+                nums.pop(i)
+            else:
+                target = nums[i]
+                i += 1
+
+        return len(nums)
 
 
 if __name__ == '__main__':
-    solution = Solution()
-    assert solution.removeDuplicates([1, 1, 2]) == [1, 2]
-    assert solution.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) == [0, 1, 2, 3, 4]
+    s: Solution = Solution()
+    assert s.removeDuplicates([1,1,2]) == 2  # [1,2,_]
+    assert s.removeDuplicates([0,0,1,1,1,2,2,3,3,4]) == 5  # [0,1,2,3,4,_,_,_,_,_]
+    assert s.removeDuplicates([1,1]) == 1  # [0,1,2,3,4,_,_,_,_,_]

4f8d251a26430dcf825690e817f9f73d47f59169
cafitac <cafitac99@gmail.com>
2024-01-24 21:57:40 +0900

    staircase

---
+++ hackerrank/staircase.py
@@ -0,0 +1,19 @@
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    for i in range(1, n+1):
+        print(" "*(n-i), end="")
+        print("#"*i)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)

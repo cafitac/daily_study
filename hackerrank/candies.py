d6c0f3056194241e005cf1ab8af2db1471943bfe
cafitac <cafitac99@gmail.com>
2024-01-25 21:50:46 +0900

    candies

---
+++ hackerrank/candies.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'candies' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def candies(n, arr):
+    candy_arr = []
+    for _ in range(n):
+        candy_arr.append(1)
+
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1] and candy_arr[i] <= candy_arr[i-1]:
+            candy_arr[i] = candy_arr[i-1] + 1
+
+    arr.reverse()
+    candy_arr.reverse()
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1] and candy_arr[i] <= candy_arr[i-1]:
+            candy_arr[i] = candy_arr[i-1] + 1
+
+    return sum(candy_arr)
+
+
+if __name__ == '__main__':
+    assert candies(3, [1,2,2]) == 4
+    assert candies(10, [2,4,2,6,1,7,8,9,2,1]) == 19
+    assert candies(8, [2,4,3,5,2,6,4,5]) == 12

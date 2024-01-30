55efb9534fe4c387f88acee9a6bb0d03ff807725
cafitac <cafitac99@gmail.com>
2024-01-30 21:50:38 +0900

    best-time-to-buy-and-sell-stock popular solution

--- leetcode/best-time-to-buy-and-sell-stock.py
+++ leetcode/best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 from typing import List
 
 
+# My solution, Best solution
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
@@ -15,9 +16,23 @@ class Solution:
         return profit
 
 
+# Popular solution
+class Solution:
+    class Solution:
+        def maxProfit(self, prices: List[int]) -> int:
+            profit = 0
+            buy_price = prices[0]
+
+            for price in prices[1:]:
+                profit = max(profit, price - buy_price)
+                buy_price = min(buy_price, price)
+
+            return profi
+
+
 if __name__ == '__main__':
     s = Solution()
-    assert s.maxProfit([7,1,5,3,6,4]) == 5
-    assert s.maxProfit([7,6,4,3,1]) == 0
-    assert s.maxProfit([2,1,2,1,0,1,2]) == 2
-    assert s.maxProfit([2,4,1]) == 2
+    assert s.maxProfit([7, 1, 5, 3, 6, 4]) == 5
+    assert s.maxProfit([7, 6, 4, 3, 1]) == 0
+    assert s.maxProfit([2, 1, 2, 1, 0, 1, 2]) == 2
+    assert s.maxProfit([2, 4, 1]) == 2

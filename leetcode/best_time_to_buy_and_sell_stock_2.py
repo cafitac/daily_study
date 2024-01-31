021d7909f30feff880bb47ab1b287b3fd9da99de
cafitac <cafitac99@gmail.com>
2024-01-31 11:29:24 +0900

    best_time_to_buy_and_sell_stock_2 Best solutions

--- leetcode/best_time_to_buy_and_sell_stock_2.py
+++ leetcode/best_time_to_buy_and_sell_stock_2.py
@@ -23,6 +23,37 @@ class Solution:
         return total_profit
 
 
+# Best Solution 1
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # It is impossible to sell stock on first day, set -infinity as initial value for cur_hold
+        cur_hold, cur_not_hold = -float('inf'), 0
+
+        for stock_price in prices:
+            prev_hold, prev_not_hold = cur_hold, cur_not_hold
+
+            # either keep hold, or buy in stock today at stock price
+            cur_hold = max(prev_hold, prev_not_hold - stock_price)
+
+            # either keep not-hold, or sell out stock today at stock price
+            cur_not_hold = max(prev_not_hold, prev_hold + stock_price)
+
+        # maximum profit must be in not-hold state
+        return cur_not_hold
+
+
+# Best Solution 2
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        price_gain = []
+
+        for idx in range(len(prices) - 1):
+            if prices[idx] < prices[idx + 1]:
+                price_gain.append(prices[idx + 1] - prices[idx])
+
+        return sum(price_gain)
+
+
 if __name__ == '__main__':
     s = Solution()
     assert s.maxProfit([7,1,5,3,6,4]) == 7
